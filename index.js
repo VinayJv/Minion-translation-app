958fa5832660cc0dc4a2ef06d4ea6c9acfb65a9c
VinayJv <110531126+VinayJv@users.noreply.github.com>
2022-10-04 01:00:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const serverURL = "https://api.funtranslations.com/translate/minion.json"
+const inputTxt = document.getElementById("input")
+const ouputTxt = document.getElementById("output")
+const translateBtn = document.getElementById("translate")
+const refreshBtn = document.getElementById("refresh")
+
+function getURL(inputt){
+    return `${serverURL}?text=${inputt}`
+
+}
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server ! try again later");
+}
+function translateTxt(){
+    // console.log("Hello")
+    console.log(inputTxt.value)
+    var url = getURL(inputTxt.value);
+    fetch(url).then(res => res.json()).then( json => {
+        console.log(json.contents.translated);
+        ouputTxt.value = json.contents.translated;
+    }).catch(errorHandler);
+}
+
+function refreshEve(){
+    location.reload();
+}
+
+
+
+
+
+
+
+translateBtn.addEventListener("click", translateTxt)
+refreshBtn.addEventListener("click", refreshEve)
